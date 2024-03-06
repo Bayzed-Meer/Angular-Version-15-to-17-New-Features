228081a3c6ff98e415379f6c9fa83547632f777f
MD Bayzed Meer <bayzed.meer@brainstation-23.com>
2024-03-06 21:53:31 +0600

    Stand alone component lazy loading added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,5 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'standAlone', loadComponent:()=> import('./angular-15/stand-alone/stand-alone.component').then(m=> m.StandAloneComponent) }
+];
